f53ca437788d7eb07418f4c5c32a4931808576c0
kevindimayuga <k.dimayuga22@gmail.com>
2023-09-12 20:13:59 -0500

    updated starter item data in models and GET routes for items

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const uploadRoutes = require('./upload');
+const itemRoutes = require('./itemRoutes');
 
 router.use('/users', userRoutes);
 router.use('/upload', uploadRoutes);
+router.use('/item', itemRoutes);
 
 module.exports = router;
---
+++ controllers/api/itemRoutes.js
@@ -0,0 +1,72 @@
+const router = require('express').Router();
+const { Item } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+// this is a GET request to get ALL items listed in the database
+router.get('/', withAuth,  async (req, res) => {
+  try {
+    const newItem = await Item.findAll();
+    // ({
+    //   ...req.body,
+    //   user_id: req.session.user_id,
+    // });
+
+    res.status(200).json(newItem);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+// this is a GET request to get one item BY ID in the database
+router.get('/:id', withAuth, async (req, res) => {
+  try {
+    const newItem = await Item.findOne();
+    ({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newItem);
+  } catch (err) {
+    console.log(err);
+    res.status(400).json(err);
+  }
+});
+
+// this is a POST request to CREATE a new item with a unique ID
+router.post('/', withAuth, async (req, res) => {
+  try {
+    const newItem = await Item.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newItem);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+// this is a DELETE request to delete an item from the database
+router.delete('/:id', withAuth, async (req, res) => {
+  try {
+    const itemData = await Item.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!itemData) {
+      res.status(404).json({ message: 'No Item found with this id!' });
+      return;
+    }
+
+    res.status(200).json(itemData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
