ca8a61267ac4471c686a9cf55f75380e193625af
sthogue <sthogue16@gmail.com>
2023-09-18 19:07:02 -0500

    fixed

--- public/js/login.js
+++ public/js/login.js
@@ -22,28 +22,6 @@ const loginFormHandler = async (event) => {
   }
 };
 
-const signupFormHandler = async (event) => {
-  event.preventDefault();
-
-  const name = document.querySelector('#name-signup').value.trim();
-  const email = document.querySelector('#email-signup').value.trim();
-  const password = document.querySelector('#password-signup').value.trim();
-
-  if (name && email && password) {
-    const response = await fetch('/api/users', {
-      method: 'POST',
-      body: JSON.stringify({ name, email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      document.location.replace('/profile');
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
 const googleFormHandler = async (event) => {
   event.preventDefault();
   document.location.replace('/auth/google');
@@ -54,10 +32,6 @@ const googleFormHandler = async (event) => {
 document
   .querySelector('#login-btn')
   .addEventListener('click', loginFormHandler);
-
-document
-  .querySelector('#signup-btn')
-  .addEventListener('click', signupFormHandler);
 
 document
   .querySelector('#google-login')
