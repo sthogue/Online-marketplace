accaf2deed5461e370d1f0e06610a142ce1a6d81
sthogue <sthogue16@gmail.com>
2023-09-19 20:10:47 -0500

    last change

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -62,6 +62,8 @@ router.get('/item/:id', async (req, res) => {
     
 
     res.render('item', {
+    // Check if the date property is valid before rendering the template
+        res.render('item', {
       ...item,
       logged_in: req.session.logged_in
     });
